bb03493bf410dd8d179d3c8eff0521751ae83b8b
Simon Tucker <brotherlogic@gmail.com>
2026-04-01 20:49:04 -0700

    Adds support files

---
+++ postCreate.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install -y  protobuf-compiler xdg-utils 
+/usr/local/go/bin/go install google.golang.org/protobuf/cmd/protoc-gen-go@latest 
+/usr/local/go/bin/go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+# Account for Ghostty
+tic -x ghostty.terminfo
+
+# Install tmux and emacs
+sudo apt-get update && sudo apt-get install -y tmux emacs
