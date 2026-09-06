9fcc94105e7ff15abc5090bcd0b9d1c2e5e0a55e
Simon Tucker <brotherlogic@gmail.com>
2026-09-05 20:01:40 -0700

    Boing (#24)
    * Fixes the container build
    
    * Also tmux

--- postCreate.sh
+++ postCreate.sh
@@ -1,10 +1,33 @@
+#!/bin/zsh
+
+export GOPATH=/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+
 sudo apt update
 sudo apt install -y  protobuf-compiler xdg-utils 
-/usr/local/go/bin/go install google.golang.org/protobuf/cmd/protoc-gen-go@latest 
-/usr/local/go/bin/go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest 
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 # Account for Ghostty
 tic -x ghostty.terminfo
 
 # Install tmux and emacs
 sudo apt-get update && sudo apt-get install -y tmux emacs
+
+# Set git identity
+git config --global user.email "brotherlogicautomation@gmail.com"
+git config --global user.name "Brotherlogic Automation"
+
+TMUX_BLOCK=$(cat << 'EOF'
+if [ -z "$TMUX" ] && [ -n "$PS1" ]; then
+  cd /workspaces/sale-description-generator
+  /workspaces/sale-description-generator/start-tmux.sh && tmux attach-session -t sale-description-generator
+fi
+EOF
+)
+
+grep -q "tmux attach-session" ~/.zshrc || echo "$TMUX_BLOCK" >> ~/.zshrc
+grep -q "tmux attach-session" ~/.bashrc || echo "$TMUX_BLOCK" >> ~/.bashrc
+
+# Ensure the session is created
+/workspaces/sale-description-generator/start-tmux.sh
